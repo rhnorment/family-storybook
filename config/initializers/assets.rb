1a736a2fc79c212f26cd513a926e506b2cb17122
rhnorment <norment@gmail.com>
2015-02-08 09:44:03 -0600

    added SendGrid for email.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
-%w(users sessions).each do |controller|
+%w(static users sessions).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.css", "#{controller}.js"]
 end

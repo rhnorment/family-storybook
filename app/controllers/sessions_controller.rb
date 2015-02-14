d75b92453faad5634661ce1f001ba04d2214e794
rhnorment <norment@gmail.com>
2015-02-14 11:57:55 -0600

    adding application layout with collapsing sidebar.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,18 +8,18 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome back, #{user.name}!"
+      flash[:success] = "Welcome back, #{user.name}!"
       redirect_to session[:intended_url] || user
       session[:intended_url] = nil
     else
-      flash.now[:alert] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'You are now signed out.'
+    redirect_to root_url, info: 'You are now signed out.'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       @user.send_activation_email
       session[:user_id] = @user.id
-      redirect_to @user, notice: 'Thanks for signing up!'
+      redirect_to @user, success: 'Thanks for signing up!'
     else
       render :new
     end
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to @user, notice: 'Account successfully updated!'
+      redirect_to @user, success: 'Account successfully updated!'
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     session[:user_id] = nil
-    redirect_to root_url, alert: 'Account successfully deleted.'
+    redirect_to root_url, info: 'Account successfully deleted.'
   end
 
   private

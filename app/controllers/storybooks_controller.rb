a70af0da20c2b899aeb29b673acb78a3d83825d7
rhnorment <norment@gmail.com>
2015-02-18 14:33:33 -0600

    storybook index, new, create tests pass.

--- app/controllers/storybooks_controller.rb
+++ app/controllers/storybooks_controller.rb
@@ -5,23 +5,23 @@ class StorybooksController < ApplicationController
   before_action     :set_storybook,         only:   [:show,   :edit, :update, :destroy]
 
   def index
-    @page_title = 'My storybook'
-    @storybooks = @user.storybooks
+    @page_title = 'My storybooks'
+    @storybooks = @user.storybooks.page params[:page]
   end
 
   def show
-    @page_title = "Showing storybook: #{@storybook.title}"
+    @page_title = "Showing: #{@storybook.title}"
   end
 
   def new
-    @page_title = 'Create a new storybook'
+    @page_title = 'Create a storybook'
     @storybook = @user.storybooks.new
   end
 
   def create
     @storybook = @user.storybooks.new(storybook_params)
     if @storybook.save
-      redirect_to storybooks_url, success: 'Your storybook was successfully created!'
+      redirect_to @storybook, success: 'Your storybook was successfully created!'
     else
       flash.now[:error] = 'There was a problem creating your storybook.  Please try again'
       render  :new
@@ -34,7 +34,7 @@ class StorybooksController < ApplicationController
 
   def update
     if @storybook.update(storybook_params)
-      redirect_to storybooks_url, success: 'Your storybook was updated.'
+      redirect_to @storybook, success: 'Your storybook was updated.'
     else
       flash.now[:error] = 'There was a problem updating your storybook.  Please try again.'
       render :edit

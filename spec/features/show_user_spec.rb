5c427745cca27595e125600683468fe5f29f11cf
rhnorment <norment@gmail.com>
2015-03-12 15:16:03 -0500

    user profile view utilizes item partials.

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -4,6 +4,7 @@ describe 'Viewing a user profile page' do
 
   before do
     @user = User.create!(user_attributes)
+    @user2 = User.create!(user_attributes(email: 'user2@example.com'))
     sign_in(@user)
   end
 
@@ -21,36 +22,46 @@ describe 'Viewing a user profile page' do
     expect(page).to have_link('Edit')
   end
 
-  it 'lists the user storybooks in the storybooks tab' do
+  it 'lists ONLY the user storybooks in the storybooks tab' do
     storybook1 = @user.storybooks.create!(storybook_attributes(title: 'Storybook 1'))
     storybook2 = @user.storybooks.create!(storybook_attributes(title: 'Storybook 2'))
+    storybook3 = @user2.storybooks.create!(storybook_attributes(title: 'Storybook 3'))
 
     visit user_url(@user)
 
     expect(page).to have_text('Storybook 1')
     expect(page).to have_text('Storybook 2')
+    expect(page).to have_text('Not published')
+    expect(page).to have_text('Started')
+    expect(page).to_not have_text('Storybook 3')
   end
 
   it 'lists the user stories in the stories tab' do
     story1 = @user.stories.create!(story_attributes(title: 'Story 1'))
     story2 = @user.stories.create!(story_attributes(title: 'Story 2'))
+    story3 = @user2.stories.create!(story_attributes(title: 'Story 3'))
 
     visit user_url(@user)
 
     expect(page).to have_text('Story 1')
     expect(page).to have_text('Story 2')
+    expect(page).to have_text('Inclusions')
+    expect(page).to have_text('Written')
+    expect(page).to_not have_text('Story 3')
   end
 
   it 'lists the user relationships in the family tab' do
-    user2 = User.create!(user_attributes(email: 'user2@example.com', name: 'User 2'))
     user3 = User.create!(user_attributes(email: 'user3@example.com', name: 'User 3'))
-    @user.invite(user2)
-    user2.approve(@user)
+    user4 = User.create!(user_attributes(email: 'user4@example.com', name: 'User 4'))
+    @user.invite(user3)
+    user3.approve(@user)
 
     visit user_url(@user)
 
-    expect(page).to have_text(user2.name)
-    expect(page).to_not have_text(user3.name)
+    expect(page).to have_text(user3.name)
+    expect(page).to have_text('Added as family member')
+    expect(page).to have_link('Remove relative')
+    expect(page).to_not have_text(user4.name)
   end
 
   it 'lists the user activities in the activity tab' do
@@ -59,8 +70,10 @@ describe 'Viewing a user profile page' do
 
     visit user_url(@user)
 
+    expect(page).to have_text(@user.name)
     expect(page).to have_text(storybook.title)
     expect(page).to have_text(story.title)
+    expect(page).to have_text('ago')
   end
 
 end

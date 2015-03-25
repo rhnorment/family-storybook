fcef028580729c71548fd87761a997fa5a3753a7
rhnorment <norment@gmail.com>
2015-03-25 18:14:38 -0500

    search specs pass

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 describe SearchController do
 
-  describe "GET 'search'" do
-    it "returns http success" do
-      get 'search'
-      response.should be_success
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+  end
+
+  context 'when not signed in' do
+
+    before do
+      session[:user_id] = nil
+    end
+
+    it 'cannot access search' do
+      get :search
+
+      expect(response).to redirect_to(new_session_url)
     end
   end
 
---
+++ spec/features/search_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe 'Search' do
+
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+    @storybook1 = @user.storybooks.create!(storybook_attributes(title: 'First Storybook',
+                                                                description: 'First storybook description.'))
+    @storybook2 = @user.storybooks.create!(storybook_attributes(title: 'Second Storybook',
+                                                                description: 'Second storybook description.'))
+    @story1 = @user.stories.create!(story_attributes(title: 'First Story', content: 'Content of first story.'))
+    @story2 = @user.stories.create!(story_attributes(title: 'Second Story', content: 'Content of second story.'))
+  end
+
+  it 'presents a search form to the user' do
+    visit search_url
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_field('query')
+  end
+
+  it 'returns an empty array with a message if an empty form is submitted' do
+    visit search_url
+
+    fill_in 'query', with: ' '
+    click_button 'Search'
+
+    expect(page).to have_text('No results to display.')
+  end
+
+  it 'returns an empty array if the search results are nil' do
+    visit search_url
+
+    fill_in 'query', with: 'hunt'
+    click_button 'Search'
+
+    expect(page).to have_text('No results to display.')
+  end
+
+  context 'when returning an array consisting items' do
+
+    it 'returns the correct array of results' do
+      visit search_url
+
+      fill_in 'query', with: 'storybook'
+      click_button 'Search'
+
+      expect(page).to have_link(@storybook1.title)
+      expect(page).to have_link(@storybook2.title)
+      expect(page).to have_link('Storybook')
+      expect(page).to have_text(@storybook1.user.name)
+      expect(page).to have_text('created')
+    end
+
+    it 'does not return an incorrect array of results' do
+      visit search_url
+
+      fill_in 'query', with: 'story'
+      click_button 'Search'
+
+      expect(page).to_not have_link(@storybook1.title)
+      expect(page).to_not have_link(@storybook2.title)
+    end
+
+  end
+
+
+end

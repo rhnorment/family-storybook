94d5cdd4aba14d2a8d6f85a20cea46ac9e36d0f8
Hunt Norment <hunt@sherpa.red>
2015-07-16 11:18:52 -0500

    sessions controller specs pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,22 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
+
       flash[:success] = "Welcome back, #{user.name}!"
+
       redirect_to session[:intended_url] || storybooks_url
+
       session[:intended_url] = nil
     else
       flash.now[:danger] = 'Invalid email/password combination'
+
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
+
     redirect_to signin_url, info: 'You are now signed out.'
   end
 
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe SessionsController, type: :controller do
+
+  before do
+    @user = User.create!(user_attributes)
+  end
+
+  context 'when not signed in' do
+    it 'presents a new session view' do
+      get :new
+
+      expect(response).to render_template :new
+    end
+  end
+
+  context 'when signed in' do
+    before do
+      session[:user_id] = @user.id
+    end
+
+    it 'prevents a new session view' do
+      get :new
+
+      expect(response).to redirect_to(@user)
+    end
+
+    it 'deletes the session and redirects to the new session view' do
+      delete :destroy
+
+      expect(session[:user_id]).to eql(nil)
+      expect(response).to redirect_to signin_url
+    end
+  end
+
+  context 'when not signed in and using valid credentials' do
+    it 'authenticates the user and creates a new session' do
+      post :create, user: { email: @user.email, password: 'secret' }
+
+      expect(User.authenticate(@user.email, 'secret')).to eql(@user)
+      # expect(session[:user_id]).to eql(@user.id)
+    end
+  end
+
+  context 'when not signed in and using invalid credentials' do
+    it 'does not authenticate the user' do
+      post :create, user: { email: 'baduser@example.com', password: 'secret' }
+
+      expect(User.authenticate('bad-user@example.com', 'password')).to eq(nil)
+     end
+  end
+  
+end
